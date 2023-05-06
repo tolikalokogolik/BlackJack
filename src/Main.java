25a07403c3cd808e882e0d3360ddff5d971c171f
Equinoxless <glebmaks502@gmail.com>
2023-05-06 18:14:31 +0300

    first commit

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BlackJack.showRules();
-        int difficulty = BlackJack.readDifficultyInput();
-        BlackJack blackjack = new BlackJack(difficulty);
-        blackjack.start();
-    }
-}
